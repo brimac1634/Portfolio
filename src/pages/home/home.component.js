ce71c8458e464a80d76f5352129942365a14cab7
Brian MacPherson <brimac1634@gmail.com>
2019-12-27 12:48:12 +0800

    added name gif and adjusted button styles

--- src/pages/home/home.component.js
+++ src/pages/home/home.component.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Parallax } from 'react-scroll-parallax';
 
+import name from '../../assets/name.gif';
 import './home.styles.scss';
 
 const Home = () => {
 	return (
 		<div className='home'>
-			<Parallax className='name' y={['-200px', '200px']}>
-		    </Parallax>
+			<img src={name} alt='name' />
 		</div>
 	)
 }
