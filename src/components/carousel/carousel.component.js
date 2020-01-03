829b729060a17ab427e9a3945bfa8003054319af
Brian MacPherson <brimac1634@gmail.com>
2020-01-03 17:56:54 +0800

    fixed the scrolling of the carousel and added more fadings and animations

--- src/components/carousel/carousel.component.js
+++ src/components/carousel/carousel.component.js
@@ -1,14 +1,22 @@
-import React, { useMemo, useEffect, useState, useRef} from 'react';
+import React, { useMemo, useEffect, useState, useRef } from 'react';
 
+import workData from '../../pages/work/work.data';
+
+import { useWindowSize } from '../../utils';
+
+import portfolio from '../../assets/portfolio_1920.gif';
 import './carousel.styles.scss';
 
-const Carousel = ({ children, showIndicator, height, setHeight }) => {
-	const [index, setIndex] = useState(0);
+const Carousel = ({ children, height, setHeight }) => {
+	const [index, setIndex] = useState(1);
 	const [translateValue, setTranslation] = useState(0);
+	const [opacity, setOpacity] = useState(1);
+	const [innerWidth] = useWindowSize();
 	const wrapper = useRef(null);
 
+	const panelWidth = useMemo(()=>innerWidth * 0.4 / 2, [innerWidth])
+
 	const lastItemOffset = useMemo(()=>{
-		console.log('im here')
 		if (!wrapper.current) return;
 		return -height + (wrapper.current.clientWidth * 0.7)
 	}, [height])
@@ -17,10 +25,19 @@ const Carousel = ({ children, showIndicator, height, setHeight }) => {
 		if (wrapper.current) setHeight(wrapper.current.scrollWidth);
 	}, [wrapper, setHeight])
 
+	useEffect(()=>{
+		if (-translateValue > innerWidth * 0.2) {
+			setIndex(Math.floor((-translateValue - innerWidth * 0.2)/ (innerWidth * 0.6)))
+		} else {
+			setIndex(1)
+		}
+	}, [translateValue, innerWidth, height])
+
 	const handleScroll = e => {
 		const newValue = translateValue + -e.deltaY;
 		if (newValue < lastItemOffset || newValue > 0) return;
 		setTranslation(newValue);
+		setOpacity(1 + ((translateValue * 100 / (innerWidth * 0.4)) * 0.01))
 	}
 
 	return (
@@ -29,6 +46,20 @@ const Carousel = ({ children, showIndicator, height, setHeight }) => {
 			onWheel={handleScroll}
 			style={{height}}
 		> 
+			<img 
+				style={{opacity}}
+				src={portfolio} 
+				alt='fullstack developer' 
+				className='port' 
+			/>
+			<div className='message' style={{opacity}}>
+	        	<div className='oval'>
+	        		<div className='line'>
+	        			<div />
+	        		</div>
+	        	</div>
+	        	<span>scroll to discover</span>
+	        </div>
 			<div 
 				className="slider-wrapper"
 				ref={wrapper}
@@ -37,14 +68,27 @@ const Carousel = ({ children, showIndicator, height, setHeight }) => {
 					WebkitTransform: `translate(${translateValue}px, -50%)`
 	            }}
 	         >
-	            {children}
+	            {
+					workData.map(({ route, image, title, description}, i) => {
+						const isVisisble = index === i;
+						return (
+							<div 
+								className={`item ${isVisisble ? 'visible' : null}`}
+								key={i}  
+							>
+								<div className='image' style={{backgroundImage: `url(${image})`}} />
+								<div className={`title ${isVisisble ? 'visible' : null}`}>
+									<h3>{title}</h3>
+									<h3>{description}</h3>
+								</div>
+							</div>
+						)
+					})
+	            }
 	        </div>
-			{
-				showIndicator &&
-				<div className='indicators'>
-					<span>{index}/{children.length}</span>
-				</div>
-			}
+			<div className={`indicators ${-translateValue > panelWidth ? 'show' : null}`}>
+				<span>{index + 1} / {workData.length}</span>
+			</div>
 		</div>
 	)
 }
--- src/pages/home/home.component.js
+++ src/pages/home/home.component.js
@@ -2,27 +2,14 @@ import React, { useState } from 'react';
 
 import Carousel from '../../components/carousel/carousel.component';
 
-import portfolio from '../../assets/portfolio_3.gif';
 import './home.styles.scss';
-
-const data = [1, 1, 1, 1, 1, 1, 1, 1 ]
 
 const Home = () => {
 	const [height, setHeight] = useState('100vh');
 	return (
 		<div className='home' style={{height}}>
 			<div className='home-content'>
-				<img src={portfolio} alt='name' />
-				<Carousel showIndicator setHeight={setHeight} height={height}>
-					{
-		              data.map((data, i) => (
-		                <div 
-		                	className='image'
-		                	key={i}  
-		                />
-		              ))
-		            }
-				</Carousel>
+				<Carousel setHeight={setHeight} height={height} />
 			</div>
 		</div>
 	)
