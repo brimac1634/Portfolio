54f5796ae75fbcd8882e4ca5231f52c80c22e4b3
Brian MacPherson <brimac1634@gmail.com>
2020-01-09 15:51:43 +0800

    added styling to first card

--- src/components/card-one/card-one.component.js
+++ src/components/card-one/card-one.component.js
@@ -4,8 +4,10 @@ import './card-one.styles.scss';
 
 const CardOne = () => (
 	<div className='card-one'>
-		<span>hey there</span>
-
+		<span>Who am I?</span>
+		<p>
+			My name is Brian MacPherson, and I am a fullstack developer based in Hong Kong. I grew up in New York where I received Finance and Spanish Literature degrees at Stony Brook University. It was there that I learned of my obsession with automation and developed a passion for learning.
+		</p>
 	</div>
 )
 export default CardOne;
