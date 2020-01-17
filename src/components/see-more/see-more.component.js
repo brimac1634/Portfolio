677f2e0160b9fda2e0673f2867d92a8128caf194
Brian MacPherson <brimac1634@gmail.com>
2020-01-17 15:43:36 +0800

    started to add watching eye

--- src/components/see-more/see-more.component.js
+++ src/components/see-more/see-more.component.js
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import { useWindowSize, useMousePosition } from '../../utils';
 
 import './see-more.styles.scss';
 
 const SeeMore = () => {
-	const [mouseX, mouseY] = useMousePosition();
-	const [innerWidth, innerHeight] = useWindowSize();
-	console.log(mouseX, mouseY)
+	const [translate, setTranslate] = useState([0, 0]);
+	const [irisWidth, setIrisWidth] = useState(0);
+	const iris = useRef(null);
+	const mousePosition = useMousePosition();
+	const windowSize = useWindowSize();
+
+	const [translateX, translateY] = translate;
+
+	useEffect(()=>{
+		if (!iris) return;
+		setIrisWidth(iris.current.clientWidth);
+	}, [iris, setIrisWidth, windowSize])
+
+	useEffect(()=>{
+		if (!iris) return;
+		let [mouseX, mouseY] = mousePosition;
+		console.log(mouseX, mouseY)
+		const [innerWidth, innerHeight] = windowSize;
+		const halfPupil = irisWidth * 0.2;
+		const translateX = ((mouseX * irisWidth) / innerWidth) - halfPupil;
+		const translateY = ((mouseY * irisWidth) / innerHeight) - halfPupil;
+		setTranslate([translateX, translateY]);
+	}, [mousePosition, windowSize, irisWidth, setTranslate])
+
 	return (
 		<div className='see-more'>
-			<div className='iris'>
-				<div className='pupil' />
+			<div className='iris' ref={iris} >
+				<div 
+					className='pupil' 
+					style={{
+						transform: `translate(${translateX}px, ${translateY}px)`,
+						WebkitTransform: `translate(${translateX}px, ${translateY}px)`,
+		            }}
+				/>
 			</div>
 		</div>
 	)
