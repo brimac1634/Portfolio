f4e12824e7717ce744bb02f3d02e1c5858d203e4
Brian MacPherson <brimac1634@gmail.com>
2020-01-21 09:47:35 +0800

    changed loading of images in work items to be individual

--- src/components/work-item/work-item.component.js
+++ src/components/work-item/work-item.component.js
@@ -1,27 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MediaQuery from 'react-responsive';
 
 import './work-item.styles.scss';
 
-const WorkItem = ({ image, title, description, setImageLoad, show, ...otherProps }) => (
-	<div className='work-item' { ...otherProps }>
-		<img 
-			className={`image ${show ? 'show' : null}`}
-			src={image} 
-			alt={title} 
-			onLoad={()=>setImageLoad()} 
-		/>
-		<MediaQuery maxWidth={575}>
-			<div className='gradient' />
-		</MediaQuery>
-		{
-			show &&
-			<div className='details'>
-				<h3>{title}</h3>
-				<span>{description}</span>
-			</div>
-		}
-	</div>
-)
+const WorkItem = ({ image, title, description, ...otherProps }) => {
+	const [loadComplete, setLoadComplete] = useState(false);
+	return (
+		<div className={`work-item ${loadComplete ? null : 'loading'}`} { ...otherProps }>
+			<img 
+				className={`image ${loadComplete ? 'show' : null}`}
+				src={image} 
+				alt={title} 
+				onLoad={()=>setLoadComplete(true)} 
+			/>
+			<MediaQuery maxWidth={575}>
+				<div className='gradient' />
+			</MediaQuery>
+			{
+				loadComplete &&
+				<div className='details'>
+					<h3>{title}</h3>
+					<span>{description}</span>
+				</div>
+			}
+		</div>
+	)
+}
 
 export default WorkItem;
--- src/pages/work/work.component.js
+++ src/pages/work/work.component.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 import workData from './work.data';
@@ -6,48 +6,36 @@ import workData from './work.data';
 import WorkItem from '../../components/work-item/work-item.component';
 import WorkDetail from '../work-detail/work-detail.component';
 
-import Card from '../../assets/Card_1000.gif';
 import './work.styles.scss';
 
-const Work = ({ match, history }) => {
-	const [imageCount, setImageCount] = useState([]);
-	const [loadComplete, setLoadComplete] = useState(false);
-	useEffect(()=>{
-		if (imageCount.length === workData.length) setLoadComplete(true);
-	}, [imageCount])
-
-	return (
-		<div className='work'>
-			<Switch>
-				<Route exact path={match.path} render={()=>(
-					<div className='work-collection'>
-						{
-							workData &&
-							workData.map(({ route, gridArea, image, title, description}, i) => (
-								<WorkItem 
-									key={title}
-									onClick={()=>history.push(`${match.path}${route}`)}
-									style={{
-										gridArea,
-										animationDelay: `${(i + 1) * 0.2}s`,
-										backgroundImage: loadComplete ? null : `url(${Card})`
-									}}
-									show={loadComplete}
-									image={image} 
-									title={title} 
-									description={description}
-									setImageLoad={()=>setImageCount([...imageCount, i])} 
-								/>
-							))
-						}
-						<div className='holder holder1'/>
-					</div>
-				)}/>
-				<Route path={`${match.path}/:work`} component={WorkDetail}/>
-				<Redirect to={match.path} />
-			</Switch>
-		</div>
-	)
-}
+const Work = ({ match, history }) => (
+	<div className='work'>
+		<Switch>
+			<Route exact path={match.path} render={()=>(
+				<div className='work-collection'>
+					{
+						workData &&
+						workData.map(({ route, gridArea, image, title, description}, i) => (
+							<WorkItem 
+								key={title}
+								onClick={()=>history.push(`${match.path}${route}`)}
+								style={{
+									gridArea,
+									animationDelay: `${(i + 1) * 0.2}s`
+								}}
+								image={image} 
+								title={title} 
+								description={description}
+							/>
+						))
+					}
+					<div className='holder holder1'/>
+				</div>
+			)}/>
+			<Route path={`${match.path}/:work`} component={WorkDetail}/>
+			<Redirect to={match.path} />
+		</Switch>
+	</div>
+)
 
 export default Work;
