c9e0a0003c3d2297cce5d0ccda8862d3d7f8b8e5
Brian MacPherson <brimac1634@gmail.com>
2020-01-22 19:20:54 +0800

    updated to work with service worker

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-
+import * as serviceWorker from './serviceWorker';
 import ScrollToTop from './components/scroll-to-top/scroll-to-top.component';
 
 import './index.css';
@@ -13,3 +13,5 @@ ReactDOM.render(
 		<App />
 	</BrowserRouter>
 , document.getElementById('root'));
+
+serviceWorker.register()
