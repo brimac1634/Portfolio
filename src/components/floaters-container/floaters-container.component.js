70d7e06407059c5d6fe0a2daf628bbe70d7a0e1e
Brian MacPherson <brimac1634@gmail.com>
2020-06-24 20:00:50 +0800

    fixed floater container to not always rerender on page resize

--- src/components/floaters-container/floaters-container.component.js
+++ src/components/floaters-container/floaters-container.component.js
@@ -6,15 +6,20 @@ import Floaters from '../floaters/floaters.component';
 import './floaters-container.styles.scss';
 
 const FloatersContainer = ({ otherClasses, show, ...otherProps }) => {
-	const [rect, setRect] = useState(null);
+	const [rect, setRect] = useState({
+		width: 0,
+		height: 0
+	});
 	const holder = useRef(null);
 	const windowSize = useWindowSize();
 
 	useEffect(()=>{
 		if (!holder.current) return;
-		const rect = holder.current.getBoundingClientRect();
-		setRect(rect)
-	}, [holder, windowSize])
+		const { width, height } = holder.current.getBoundingClientRect();
+		if (width !== rect.width || height !== rect.height) {
+			setRect({ width, height });
+		}
+	}, [rect, holder, windowSize])
 
 	return (
 		<div className={`floaters-container ${otherClasses} ${show ? 'show' : null}`} ref={holder} { ...otherProps }>
