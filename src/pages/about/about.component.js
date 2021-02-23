d11d22cc440a6f02ab04b60d6227bc1f4fdf4bb0
Brian MacPherson <brimac1634@gmail.com>
2021-02-23 16:36:10 +0800

    updated information in about page

--- src/pages/about/about.component.js
+++ src/pages/about/about.component.js
@@ -10,14 +10,11 @@ const About = () => (
 		<div className='content'>
 			<p className='intro'>
 				Hi, I'm Brian MacPherson.
-				<br />A Full-Stack Developer
+				<br />A fullstack developer
 				<br />based in Hong Kong
 			</p>
 			<p className='details'>
-				I was born and raised in New York where I eventually attended university at Stony Brook, receiving degrees in Finance and Spanish Literature. It was there that I learned of my obsession with efficiency and automation, ultimately leading me to become a programmer. Today, I live and work in Hong Kong as a Project Manager/Fullstack Developer. 
-				<br />
-				<br />
-				I have an immense passion for learning, problem solving, and building projects. When I'm not working on a project, I'm digging into the latest technologies, so that I can make well informed decisions for future projects.
+				I have an obsession for learning, problem solving, automating, and building great projects. When I'm not working on a project, I'm digging into the latest technologies, so that I can make the best decisions for future projects.
 			</p>
 			<p className='interest'>
 				Interested in working together? 
